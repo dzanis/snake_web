b9830f7d1263ff89b37416ddb49dc4a9ca8b68c9
dzanis <zanisdukalskis@gmail.com>
2019-11-17 14:14:33 +0200

    try fix resize orientationchange 6

--- Touch.js
+++ Touch.js
@@ -11,7 +11,7 @@ Touch.CONTROLS_CSS_NOFILL = 'opacity:0.5; z-index: 11000; border-style: dashed; 
 Touch.CONTROLS_CSS =  'background-color: grey; ' + Touch.CONTROLS_CSS_NOFILL;
 Touch.DPAD_BUTTON_WIDTH_PERCENT = 18;
 Touch.DPAD_BUTTON_HEIGHT_PERCENT = 12;
-Touch.DPAD_BUTTON_WIDTH_SKIP_PERCENT = 0;
+Touch.ALIGN_RIGHT = 0;
 
 
 Touch.dpad = {}; // map of dpad control objects
@@ -32,17 +32,17 @@ Touch.init = function()
         //console.log('orientation landscape');
 		Touch.DPAD_BUTTON_WIDTH_PERCENT = 25 / 3;
 		Touch.DPAD_BUTTON_HEIGHT_PERCENT = 18;
-		Touch.DPAD_BUTTON_WIDTH_SKIP_PERCENT = 100 - (Touch.DPAD_BUTTON_WIDTH_PERCENT*3);
+		Touch.ALIGN_RIGHT = 100 - (Touch.DPAD_BUTTON_WIDTH_PERCENT*3);
         break; 
       default:
         //console.log('orientation portrait');
 		Touch.DPAD_BUTTON_WIDTH_PERCENT = 18;
 		Touch.DPAD_BUTTON_HEIGHT_PERCENT = 25 / 3;
-		Touch.DPAD_BUTTON_WIDTH_SKIP_PERCENT = 50 - (Touch.DPAD_BUTTON_WIDTH_PERCENT+Touch.DPAD_BUTTON_WIDTH_PERCENT/2);
+		Touch.ALIGN_RIGHT = 100 - (Touch.DPAD_BUTTON_WIDTH_PERCENT*3);
         break; 
     }
   // remove button 
-  var btnName = ['up','left','right','down','center'];
+  var btnName = ['up','left','right','down','space'];
   btnName.forEach(function(btn){
 		var el = document.getElementById(btn); 
 		if(el != null) el.outerHTML = ""; 	});	
@@ -67,7 +67,7 @@ Touch.init = function()
     // no op
   });
   Touch.dpad['up'].style.bottom = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 2) + '%';
-  Touch.dpad['up'].style.left = Touch.DPAD_BUTTON_WIDTH_SKIP_PERCENT + Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
+  Touch.dpad['up'].style.left = Touch.ALIGN_RIGHT + Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
 
 
 
@@ -76,7 +76,7 @@ Touch.init = function()
   }, function() {
     // no op
   });
-  Touch.dpad['left'].style.left = Touch.DPAD_BUTTON_WIDTH_SKIP_PERCENT + '%';
+  Touch.dpad['left'].style.left = Touch.ALIGN_RIGHT + '%';
   Touch.dpad['left'].style.bottom = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
   
 
@@ -86,7 +86,7 @@ Touch.init = function()
      // no op
   });
   Touch.dpad['right'].style.bottom = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  Touch.dpad['right'].style.left = (Touch.DPAD_BUTTON_WIDTH_SKIP_PERCENT + Touch.DPAD_BUTTON_WIDTH_PERCENT * 2) + '%';
+  Touch.dpad['right'].style.left = (Touch.ALIGN_RIGHT + Touch.DPAD_BUTTON_WIDTH_PERCENT * 2) + '%';
 
 
 
@@ -95,34 +95,18 @@ Touch.init = function()
   }, function() {
     // no op
   });
-  Touch.dpad['down'].style.left= Touch.DPAD_BUTTON_WIDTH_SKIP_PERCENT + Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
+  Touch.dpad['down'].style.left= Touch.ALIGN_RIGHT + Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
 
 
 
   // create a dummy button in the middle to accept touchstarts. Override the default CSS for no red colour.
-  Touch.dpad['center'] = Touch.createDPadButton('center', function() {
+  Touch.dpad['space'] = Touch.createDPadButton('space', function() {
 	  // no op
   }, function() {
-	  if(Touch.needFullScreen)
-	{
-		if(Touch.twaice)
-		{
-			fullscreen();
-			Touch.needFullScreen = false;
-			Touch.twaice = undefined;
-		}		
-		Touch.twaice = true;		
-	}
-	else
-	{
-		Module._keyUp(32); // space
-	}
-    
+		Module._keyUp(32); // space   
   });
-  Touch.dpad['center'].style.left= Touch.DPAD_BUTTON_WIDTH_SKIP_PERCENT + Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  Touch.dpad['center'].style.bottom = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  Touch.dpad['center'].style.textAlign  = 'center'; 
-  Touch.dpad['center'].innerText = 'SPACE';
+  Touch.dpad['space'].style.textAlign  = 'center'; 
+  Touch.dpad['space'].innerText = 'SPACE';
 };
 
 // DPad buttons are divs with explicit press/unpress functions.
@@ -222,20 +206,26 @@ Touch.getIdOfTouchedElement = function(touchEvent)
 // fire button is a bit simpler, no need for any touchmove for sliding fingers
 Touch.initFireButton = function()
 {
+	var el = document.getElementById('fireButton'); 
+		if(el != null) el.outerHTML = "";
+	
   Touch.fireButton = document.createElement('div');
   Touch.fireButton.id = 'fireButton';
-  Touch.fireButton.style.cssText = Touch.CONTROLS_CSS;
-  Touch.fireButton.style.width = '40%';
-  Touch.fireButton.style.height = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 3) + '%';
+  Touch.fireButton.style.cssText = 'background-color: white;';
+  Touch.fireButton.style.width = '25%';
+  Touch.fireButton.style.height = '25%';
   Touch.fireButton.style.position = 'absolute';
-  Touch.fireButton.style.bottom = '0px';
-  Touch.fireButton.style.right = '0px';
+  Touch.fireButton.style.bottom = '37%';
+  Touch.fireButton.style.right = '37%';
+  Touch.fireButton.style.textAlign  = 'center'; 
+  Touch.fireButton.innerText = 'Touch to fullscreen.\n\n If you use landscape orientation better use full screen';
 
   Touch.fireButton.press = function() {
-    Keys.shooting = true;
+	  // no op
   };
   Touch.fireButton.unpress = function() {
-    Keys.shooting = false;
+    Touch.fullScreen();
+	document.getElementById('fireButton').outerHTML = "";
   };
 
   Touch.fireButton.addEventListener('touchstart', function(event) {
@@ -253,3 +243,54 @@ Touch.initFireButton = function()
 
   document.body.appendChild(Touch.fireButton);
 };
+
+// функция вкючает полноэкранность только если уже было
+// взаимодействие пользователя с дисплеем
+Touch.startFullScreen = function(){
+		
+	if(Touch.isFullScreen())
+	{
+		//console.log('isFullScreen');
+		return;
+	}
+		// ставим на паузу и ожидаем нажатий
+		Module._hide();
+		Touch.initFireButton();
+}
+
+// https://code-boxx.com/lock-screen-orientation/ 
+Touch.fullScreen = function(){
+  // Kind of painful, but this is how it works for now
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+  }
+}
+
+Touch.exitFullScreen = function(){
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+Touch.isFullScreen = function(){
+    var result =
+        document.fullscreenElement ||
+        document.mozFullScreenElement ||
+        document.webkitFullscreenElement ||
+        document.msFullscreenElement;
+
+    if(result) return true;
+}
+
